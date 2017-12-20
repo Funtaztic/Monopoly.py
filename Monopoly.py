6877ca43ae3abfdf8028c31e06c16b0cd96165d6
Funtaztic <abelszabolcsi@gmail.com>
2017-12-20 10:24:17 +0100

    Initial commit: def wait(sec) def test_wait() def basic_money_function()

---
+++ Monopoly.py
@@ -0,0 +1,48 @@
+##############################################################################
+####This should be the digital version of the classic boardgame: Monopoly.####
+##############################################################################
+#Make it for 1 player first, or start development with multiple players?
+
+##############################################################################
+#Make a wait() function with a configurable parameter, like: wait(1) or wait(2).
+#Source: https://www.pythoncentral.io/pythons-time-sleep-pause-wait-sleep-stop-your-code/
+def wait(sec):
+    time.sleep(sec)
+
+def test_wait():
+    print('1')
+    wait(1)
+    print('2')
+    wait(2)
+    print('3')
+    wait(3)
+
+#test_wait()
+
+##############################################################################
+
+def basic_money_function():
+    global MONEY
+    print("you have ", MONEY, "money")
+    print("What do you want to do with the money?")
+    print("1. Give")
+    print("2. Take")
+
+    answer = input().lower()
+    if answer == '1':
+        MONEY -= 100
+        answer = 'none'
+        basic_money_function()
+    elif answer == '2':
+        MONEY += 100
+        answer = 'none'
+        basic_money_function()
+    else:
+        print("Okay, no transactions then.")
+        #this function call is not needed in the long run, can be deleted.
+        #it may be replaced with a return?
+        basic_money_function()
+
+basic_money_function()
+
+##############################################################################
