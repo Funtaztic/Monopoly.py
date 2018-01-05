72e8c666c55b54752b11b0c2e72649ef49e2bd86
Funtaztic <abelszabolcsi@gmail.com>
2018-01-05 21:59:30 +0100

    ALMOST WORKING!
    MAIN_MOVE_FUNCTION(*dict_for_active_stuff)

--- Person_class.py
+++ Person_class.py
@@ -1,25 +1,41 @@
 #####################################################
+import math
+def dice_roll():
+    from random import randint
+    result = (randint(1,6))
+    return(result)
+
 class Player(object):
 
-  def __init__(self,name,money):
-    self.name       = name
-    self.money      = money
-    self.properties_list = []
+  def __init__(self,name,money,position):
+    self.name               = name
+    self.money              = money
+    self.position           = position
+    self.properties_list    = []
 
   def intro(self):
-    print('Player name:\t\t', self.name)
-    print('Player money:\t\t', self.money)
+    print('Player name:\t\t',     self.name)
+    print('Player money:\t\t',    self.money)
+    print('Player position:\t',   self.position)
     print('Player properties:\t', self.properties_list)
     print('******************************************')
 
   def get(self,new_property):
     self.properties_list.append(new_property)
-    print(self.name, 'have bought', new_property)
+    print(self.name, '\thave bought\t', new_property)
+    print('******************************************')
+
+  # def roll_the_dice(self,position):
 
 #####################################################
-Player1 = Player('John', 100)
-Player2 = Player('James',100)
-Player3 = Player('Jeno', 100)
+Player1 = Player('John', 100, 0)
+Player2 = Player('James',100, 0)
+Player3 = Player('Jeno', 100, 0)
+players = []
+players.append(Player1.name)
+players.append(Player2.name)
+players.append(Player3.name)
+print('Player list:',players)
 #####################################################
 class Field(object):
 
@@ -36,27 +52,48 @@ class Field(object):
 
   def buy(self,new_owner_name, new_owner_money):
     self.owner = new_owner_name
-
 #####################################################
 Field1  = Field('Miskolc',    10,   'None')
 Field2  = Field('Debrecen',   15,   'None')
 Field3  = Field('Pest',       20,   'None')
 Field4  = Field('Röcsöge',    5,    'None')
 #####################################################
+dict_for_active_stuff = []
+
+def ACTIVATOR_FUNCTION(active_player, active_player_position, active_field):
+  # for player in players:
+  dict_for_active_stuff.append(active_player)
+  dict_for_active_stuff.append(active_player_position)
+  dict_for_active_stuff.append(active_field)
+
 #####################################################
+field_list = [Field1.name, Field2.name, Field3.name]
+
+def MAIN_MOVE_FUNCTION(player, position, field):
+  print('Hi',player)
+  print('You are on field',field_list[position-1])
+  print('Do you want to buy',field)
+
+# which_one = int(input("What month (1-12)? "))
+# months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+#           'August', 'September', 'October', 'November', 'December']
+
+# if 1 <= which_one <= 12:
+#     print("The month is", months[which_one - 1])
+
 #####################################################
 Player1.intro()
-Player2.intro()
-Player3.intro()
+# Player2.intro()
+# Player3.intro()
 
-# Field1.intro()
+Field1.intro()
 # Field2.intro()
 # Field3.intro()
 #####################################################
 Player1.get(Field1.name)
 Player2.get(Field2.name)
 Player3.get(Field3.name)
-Player3.get(Field4.name)
+# Player3.get(Field4.name)
 #####################################################
 Player1.intro()
 Player2.intro()
@@ -73,12 +110,18 @@ def do_you_have_it_question_for_everybody():
     do_you_have_it(Field2.name, Player1.properties_list,Player1.name)
     do_you_have_it(Field3.name, Player1.properties_list,Player1.name)
 
-    do_you_have_it(Field1.name, Player2.properties_list,Player2.name)
-    do_you_have_it(Field2.name, Player2.properties_list,Player2.name)
-    do_you_have_it(Field3.name, Player2.properties_list,Player2.name)
+    # do_you_have_it(Field1.name, Player2.properties_list,Player2.name)
+    # do_you_have_it(Field2.name, Player2.properties_list,Player2.name)
+    # do_you_have_it(Field3.name, Player2.properties_list,Player2.name)
 
-    do_you_have_it(Field1.name, Player3.properties_list,Player3.name)
-    do_you_have_it(Field2.name, Player3.properties_list,Player3.name)
-    do_you_have_it(Field3.name, Player3.properties_list,Player3.name)
+    # do_you_have_it(Field1.name, Player3.properties_list,Player3.name)
+    # do_you_have_it(Field2.name, Player3.properties_list,Player3.name)
+    # do_you_have_it(Field3.name, Player3.properties_list,Player3.name)
 
 do_you_have_it_question_for_everybody()
+
+ACTIVATOR_FUNCTION(Player1.name, Player1.position, Field1.name)
+print(dict_for_active_stuff)
+
+#This does not work perfectly yet, but it is close.
+MAIN_MOVE_FUNCTION(*dict_for_active_stuff)
