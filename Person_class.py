4ef93e9638d44067c86278c235ff172b7bd50de3
Funtaztic <abelszabolcsi@gmail.com>
2018-01-05 16:18:26 +0100

    Person class working beautifullay

--- Person_class.py
+++ Person_class.py
@@ -1,25 +1,25 @@
 #####################################################
 class Person(object):
 
-  def __init__(self,name,age,town,height):
-    self.name   = name
-    self.age    = age
-    self.town   = town
-    self.height = height
+  properties_list = []
+
+  def __init__(self,name,money):
+    self.name       = name
+    self.money      = money
 
   def intro(self):
-    print('My name is',self.name)
-    print('My age is', self.age)
-    print('My town is', self.town)
-    print('My height is', self.height, 'cm')
-    print('**********************************')
+    print('Player name:\t\t', self.name)
+    print('Player money:\t\t', self.money)
+    print('Player properties:\t', self.properties_list)
+    print('******************************************')
 
-  def grow(self):
-    self.height += 10
+  def get(self,new_property):
+    self.properties_list.append(new_property)
+
 #####################################################
-Person1 = Person('John',27,'New York',189)
-Person2 = Person('James',47,'New York',175)
-Person3 = Person('Jeno',21,'New Jersey',199)
+Person1 = Person('John', 100)
+Person2 = Person('James',100)
+Person3 = Person('Jeno', 100)
 #####################################################
 class Field(object):
 
@@ -29,39 +29,36 @@ class Field(object):
     self.owner  = owner
 
   def intro(self):
-    print('My name is',self.name)
-    print('My price is', self.price)    
-    print('My owner is', self.owner)
-    print('**********************************')
+    print('**Property name\t\t',self.name)
+    print('**Property price\t', self.price)    
+    print('**Property owner\t', self.owner)
+    print('******************************************')
 
-  def buy(self,new_owner):
-    self.owner = new_owner
+  def buy(self,new_owner_name, new_owner_money):
+    self.owner = new_owner_name
+
 #####################################################
 Field1  = Field('Miskolc',10,'None')
 Field2  = Field('Debrecen',15,'None')
 Field3  = Field('Pest',20,'None')
 #####################################################
 Person1.intro()
-Person2.intro()
-Person3.intro()
+# Person2.intro()
+# Person3.intro()
 
-Person1.grow()
-Person2.grow()
-Person3.grow()
-
+Field1.intro()
+# Field2.intro()
+# Field3.intro()
+#####################################################
+Field1.buy(Person1.name, Person1.money)
+Person1.get(Field1.name)
+# Field2.buy(Person2.name,Person2.money)
+# Field3.buy(Person3.name,Person3.money)
+#####################################################
 Person1.intro()
-Person2.intro()
-Person3.intro()
-#####################################################
+# Person2.intro()
+# Person3.intro()
+
 Field1.intro()
-Field2.intro()
-Field3.intro()
-#####################################################
-print(Person1.name)
-Field1.buy(Person1.name)
-Field2.buy(Person2.name)
-Field3.buy(Person3.name)
-#####################################################
-Field1.intro()
-Field2.intro()
-Field3.intro()
+# Field2.intro()
+# Field3.intro()
